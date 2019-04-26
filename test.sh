a65da87603ddec014ad7c460240a42c7baecc838
Alexander Lenail <alex@lenail.org>
2019-04-26 11:26:46 -0400

    Outputs are collected from the /output directory

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-bcftools view -r chr11:10000-100000 /data/CGND-HDA-01332.haplotypeCalls.er.raw.vcf.gz > selected_variants.vcf
+mkdir /output && bcftools view -r chr11:10000-100000 /data/CGND-HDA-01332.haplotypeCalls.er.raw.vcf.gz > /output/selected_variants.vcf
 
---
+++ test2.sh
@@ -0,0 +1,1 @@
+mkdir /output && bcftools index -t CGND-HDA-01332.haplotypeCalls.er.raw.vcf.gz -o  /output/CGND-HDA-01332.haplotypeCalls.er.raw.vcf.gz.tbi
